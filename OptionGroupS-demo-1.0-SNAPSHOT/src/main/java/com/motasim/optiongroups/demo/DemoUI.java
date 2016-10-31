8927f867e1dbbe88d37e52c823f1457b3bd6903e
Motasim <Motasim.gumaa@gmail.com>
2016-10-31 22:16:01 +0200

    DEMO

---
+++ OptionGroupS-demo-1.0-SNAPSHOT/src/main/java/com/motasim/optiongroups/demo/DemoUI.java
@@ -0,0 +1,51 @@
+package com.motasim.optiongroups.demo;
+
+import com.motasim.optiongroups.OptionGroupS;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import java.util.Arrays;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+
+@Theme("demo")
+@Title("OptionGroupS Add-on Demo")
+@SuppressWarnings("serial")
+public class DemoUI extends UI
+{
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+        // Initialize our new UI component
+        // Initialize our new UI component
+        final OptionGroupS component = new OptionGroupS("Please choose the features you want to activate: ", Arrays.asList("GPS", "Google Maps", "RFID", "TRY THIS", "HELLO"));
+       
+
+        component.addValueChangeListener(e -> Notification.show("Value changed:",
+                String.valueOf(e.getProperty().getValue()),
+                Type.TRAY_NOTIFICATION));
+ 
+ 
+        // Show it in the middle of the screen
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setStyleName("demoContentLayout");
+        layout.setSizeFull();
+        layout.addComponent(component);
+        layout.setComponentAlignment(component, Alignment.MIDDLE_CENTER);
+        setContent(layout);
+    }
+}
